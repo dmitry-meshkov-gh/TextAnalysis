4d9844d71e65708d3bd19a7c93e7161ac3134ba4
dmitry_meshkov@outlook.com <dmitry_meshkov@outlook.com>
2022-11-21 19:31:03 +0300

    Добавлен метод для получения списка биграмов

--- FrequencyAnalysisTask.cs
+++ FrequencyAnalysisTask.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace TextAnalysis
 {
@@ -7,8 +8,26 @@ namespace TextAnalysis
         public static Dictionary<string, string> GetMostFrequentNextWords(List<List<string>> text)
         {
             var result = new Dictionary<string, string>();
-            //...
+            Dictionary<string, string> bigramList = GetBigramList(text);
+
             return result;
         }
-   }
+
+        private static Dictionary<string, string> GetBigramList(List<List<string>> text)
+        {
+            var bigramList = new Dictionary<string, string>();
+            foreach(List<string> sentence in text) // Обход по предложениям к тексте
+            {
+                if(sentence.Count > 3) //Если в предложении больше 3 значений
+                {
+                    for (int i = 0; i < sentence.Count - 1; i++) //Количество ключей на 1 меньше количества слов в предложении
+                    {
+                        if (!bigramList.ContainsKey(sentence[i])) // Если ключа нет, то добавить ключ-значение
+                            bigramList.Add(sentence[i], sentence[i + 1]);
+                    }
+                }
+            }
+            return bigramList;
+        }
+    }
 }
